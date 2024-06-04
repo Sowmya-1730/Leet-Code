2edb6266992d52570450ccd425f23b99a4119a13
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2024-06-04 18:09:54 +0530

    Time: 47 ms (5.13%) | Memory: 16.6 MB (80.97%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -17,16 +17,6 @@ class Solution:
             for i in d:
                 if d[i]%2==0:
                     sm+=d[i]
-                # elif d[i]==1 and f==0:
-                #     sm+=d[i]
-                #     f=1
-                #     odd=1
-                # elif d[i]==1 and f==1:
-                #     continue
-                #     odd=1
-                # else:
-                #     sm+=d[i]-1
-                #     odd=1
                 else:
                     odd=1
                     if d[i]==1 and f==0:
@@ -38,5 +28,4 @@ class Solution:
                         sm+=d[i]-1
             if f==0 and odd==1: 
                 sm+=1
-            print(d)
             return sm
