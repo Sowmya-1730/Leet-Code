4ac44f9cd2a3800c52b563481ec72c5fc3107c50
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2025-02-10 10:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.42%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res;
+        for(int i=0;i<s.size();i++)
+        {
+            if(isdigit(s[i])){
+                res.pop_back();
+            }
+            else{
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
