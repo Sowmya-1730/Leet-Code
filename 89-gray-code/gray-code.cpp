ba5338b94310d64b4f1077d6cfe2d3952670f5e6
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2025-06-03 10:32:48 +0530

    Time: 3 ms (23.43%) | Memory: 14.8 MB (35.44%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int>v;
+        for(int i=0;i<pow(2,n);i++)
+        {
+            v.push_back((i^(i>>1)));
+        }
+        return v;
+    }
+};
