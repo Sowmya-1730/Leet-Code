d1f60c37c9b8dcec7782339fd5639a7a6804680e
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2026-05-20 12:22:19 +0530

    Time: 233 ms (82.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT s.user_id,
+    ROUND(COUNT(c.action) FILTER (WHERE c.action = 'confirmed') / COUNT(*) :: decimal, 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
